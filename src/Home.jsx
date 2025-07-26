4d51e8b07b5cafc7121f52c0fc85753a08660e51
yago-vasco <yago.rc.04@gmail.com>
2025-07-26 11:07:23 +0200

    build(ui): creación de la home page

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import './Home.css';
 
 function Home() {
-    return (<>
-        <p>Hola soy el home</p>
-    </>
+    return (
+        <>
+            <header className='head'>
+                <h1 className='title'>FinZen</h1>
+                <div className='buttons'>
+                    <button className='login'>Iniciar Sesion</button>
+                    <button className='signup'>Crear Cuenta</button>
+                </div>
+            </header>
+            <section className='hero' >
+                <h1 className='hero-title'>Transforma tu relación con el dinero</h1>
+                <p className='hero-description'>FinZen es la plataforma integral que te ayuda a gestionar tus finanzas personales con inteligencia artificial, análisis avanzado y herramientas intuitivas para alcanzar tus metas financieras.</p>
+                <button className='signup'> Comenzar Gratis →</button>
+            </section>
+        </>
     );
 }
 
