6367b9e436743a324ba89b16351cfa2b489d1b21
yago-vasco <yago.rc.04@gmail.com>
2025-07-28 16:58:53 +0200

    feat(dashboard): creación de ruta de la dashboard

--- src/views/App.jsx
+++ src/views/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Home.jsx';
 import Login from './Login.jsx';
+import Dashboard from './dashboard.jsx'; // Assuming you have a Dashboard component
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   );
