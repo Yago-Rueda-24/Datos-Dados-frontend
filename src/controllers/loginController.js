a051b8df2df98d2e0fddfca89b265300bd957ce8
yago-vasco <yago.rc.04@gmail.com>
2025-07-30 12:29:13 +0200

    build(controller): implementación del controller de signup
    La función hace una pequeña comporbación de errores del lado del cliente y envia los datos a la función encargada de hacer la petición

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,4 +1,6 @@
-import { loginRequest } from "../api/loginAPI";
+import { loginRequest, signupRequest } from "../api/loginAPI";
+
+
 export async function handleLogin(username, password) {
     // Aquí puedes validar o transformar si hace falta
     try {
@@ -7,6 +9,19 @@ export async function handleLogin(username, password) {
         console.log(localStorage.getItem('token'));
     } catch (err) {
         console.error('Error en login:', err.message);
+        throw err;
+    }
+}
+
+export async function handleSignup(username, password, confirmPassword) {
+    if (password !== confirmPassword) {
+        throw new Error('Las contraseñas no coinciden');
+    }
+    try {
+        const success = await signupRequest(username, password,confirmPassword);
+        return success; 
+    } catch (err) {
+        console.error('Error en signup:', err.message);
         throw err;
     }
 }
