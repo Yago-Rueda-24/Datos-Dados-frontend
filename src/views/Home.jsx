790d70bdab656b71572c947af9af300a9394e58f
yago rueda <yago.rc.04@gmail.com>
2025-08-19 11:05:18 +0200

    Cambios en la UI del home

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Home.css';
 import { Link } from 'react-router-dom';
+import CategoryCard from '../components/Categorycard';
 
 function Home() {
     return (
@@ -24,6 +25,17 @@ function Home() {
                     <button className='signup'> Comenzar Gratis →</button>
                 </Link>
 
+            </section>
+            <section className='cards'>
+                <h1 className='card-title'> Herramientas Épicas para Aventureros</h1>
+                <p className='card-description'> Todo lo que necesitas para <span className='hero-title-resaltado'> crear, gestionar y vivir</span> aventuras inolvidables en el mundo de D&D.</p>
+                <div className='cardGrid'>
+                    <CategoryCard title={"Gestión de Partidas"} description={"Organiza sesiones, rastrea progreso y mantén a tu grupo conectado con herramientas intuitivas."}></CategoryCard>
+                    <CategoryCard title={"Base de Datos Completa"} description={"Accede a hechizos, clases, razas y objetos oficiales y a contenido creado por la comunidad."}></CategoryCard>
+                    <CategoryCard title={"Creador de Personajes"} description={"Diseña personajes únicos con nuestro editor avanzado y compártelos con otros jugadores."}></CategoryCard>
+                    <CategoryCard title={"Dados Virtuales"} description={"Sistema de dados integrado con historial, modificadores automáticos y efectos visuales."}></CategoryCard>
+
+                </div>
             </section>
         </>
     );
