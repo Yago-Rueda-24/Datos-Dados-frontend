23667392316cc20622efed68e42db5ae9bff3dfe
yago-vasco <yago.rc.04@gmail.com>
2025-08-21 00:46:37 +0200

    Modificación de las rutas de la aplicación

--- src/views/App.jsx
+++ src/views/App.jsx
@@ -6,6 +6,7 @@ import Dashboard from './Dashboard.jsx';
 import Signup from './Signup.jsx';
 import Wiki from './Wiki.jsx';
 import Wikicategory from './Wikicategory.jsx';
+import SpellPage from './Wiki/SpellPage.jsx';
 
 function App() {
   return (
@@ -16,7 +17,8 @@ function App() {
         <Route path="/signup" element={<Signup />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/wiki" element={<Wiki />} />
-        <Route path="/wikicategory" element={<Wikicategory />} />
+        <Route path="/wiki/spells" element={<Wikicategory />} />
+        <Route path="/wiki/spell/:id" element={<SpellPage />} />
       </Routes>
     </BrowserRouter>
   );
